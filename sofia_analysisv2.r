88cf75b3e14ab1a6b57ea078af0b3498ad01a937
Sofia Rivas <sfrivas415@gmail.com>
2025-10-15 16:28:24 -0700

    working - quad

--- sofia_analysisv2.r
+++ sofia_analysisv2.r
@@ -1,3 +1,4 @@
+#just gonad data, no spawning data
 rm(list=ls())
 
 # Load First --------------------------------------------------------------
@@ -7,129 +8,84 @@ librarian::shelf(tidyverse,here, janitor, googlesheets4, lubridate, splitstacksh
                  googledrive,googlesheets4,httpuv,dplyr,ggplot2,pwr2, tidyr, broom)
 
 
-# Data Sets ---------------------------------------------------------------
-
-spawn_raw <- read_sheet("https://docs.google.com/spreadsheets/d/11DLr38iVRDcvWiDoBY1hl_Cn5M9oAxDwBRQsx_eMbHk/edit?usp=sharing") 
+# Load Data Sets ---------------------------------------------------------------
 
 gonad_raw <- read_sheet("https://docs.google.com/spreadsheets/d/18R1F5KkILws3e-8CYz83BdGYMG8zeuvOIeKcaSXeqC4/edit?gid=1621016945#gid=1621016945")
-
 
 load("/Users/sofiarivas/Downloads/kelp_recovery_data (1).rda")
 
 load("/Users/sofiarivas/Downloads/lda_patch_transitionsv2.rda")
 
+# Reworking Data ----------------------------------------------------------
 
-#delete unnecessary rows
-spawn_working <- subset(spawn_raw, select = -c(Data_Enterer,Gonad_Mass_total,Spawn_Mass_total,Treatment))
+#includes year in site_id, not used for joining patch types
+gonad_working <- gonad_raw %>% 
+  select("date_collected","survey_type","site_number","site_type","zone",
+         "transect","species","sex","test_diameter_mm","gonad_mass_g") %>%
+  filter(species %in% c("purple_urchin","purple_urchins")) %>%
+  mutate(species = "purple_urchin")%>%
+  filter(!is.na(gonad_mass_g)) %>% 
+  mutate(year = year(date_collected),
+         #year2 = is.character(year),
+    site_id = paste(site_number,site_type,zone,year)) %>% 
+  filter(gonad_mass_g<30) %>% 
+  filter(survey_type == "Recovery")
 
-gonad_working <- subset(gonad_raw, select = -c(Institution,Name_of_Data_Enterer,Date_Fixed,Treatment,Soft_Tissue_Mass_g,Notes,Date_Entered)) %>%
-  filter(Species %in% c("PUR", "purple_urchin")) %>%
-  mutate(Species = "purple_urchin")%>%
-  filter(!is.na(Gonad_Mass_g)) %>% 
-  mutate(site_id = paste(Site_Number, Transect)) %>% 
-  filter(year(Date_Collected) == 2025,
-         Gonad_Mass_g<30) %>% 
-  filter(!str_starts(Site_Number, "MAR")) %>% 
-  mutate(site_id = gsub("DEEP$", "Deep", site_id)) %>%
-  mutate(site_id = gsub("SHALLOW$", "Shallow", site_id))
+#used for joining patch types
+gonad_working2 <- gonad_raw %>% 
+  select("date_collected","survey_type","site_number","site_type","zone",
+         "transect","species","sex","test_diameter_mm","gonad_mass_g") %>%
+  filter(species %in% c("purple_urchin","purple_urchins")) %>%
+  mutate(species = "purple_urchin")%>%
+  filter(!is.na(gonad_mass_g)) %>% 
+  mutate(year = year(date_collected),
+         #year2 = is.character(year),
+         site_id = paste(site_number,site_type,zone)) %>% 
+  filter(gonad_mass_g<30) %>% 
+  filter(survey_type == "Recovery")
 
-
+#patch types
+patch_types <- transitions_tbl_constrained %>%
+  mutate(site_id = paste (site,site_type,zone)) %>% 
+  rename_with(~ gsub("patch_", "", .x))
+  pivot_longer(cols = c(`2024`, `2025`), names_to = "year", values_to = "new_zone") %>%
+  mutate(year = as.numeric(year))
+  
+#joined gonad data with patch types 
+gonad_patch_joined <- left_join(gonad_working2, patch_types, by = "site_id") %>% 
+  mutate(site_type.x = if_else(year == 2024, `2024`, `2025`)) %>% 
+  mutate(site_id = paste(site_id, year))
+  
 urchin_sizefq_1 <- urchin_sizefq %>%
   filter(species=="Purple") %>% 
-  mutate(site_id = paste(site, site_type,zone)) %>% 
-  filter(year(survey_date) == 2025) 
+  mutate(year = year(survey_date)) %>% 
+  mutate(site_id = paste(site, site_type,zone)) 
 
-#make values numeric
-spawn_working$Spawn_Mass_g <- as.numeric(as.character(spawn_working$Spawn_Mass_g))
-spawn_working$Spawn_Mass_false <- as.numeric(as.character(spawn_working$Spawn_Mass_false))
-spawn_working$Animal_24hr_Mass_g <- as.numeric(spawn_working$Animal_24hr_Mass_g)
+#joined urchin size frequency data with patch types
+urchin_sizefq_joined <- left_join(urchin_sizefq_1, patch_types, by = "site_id") %>% 
+  mutate(site_type.x = if_else(year == 2024, `2024`, `2025`)) %>% 
+  mutate(site_id = paste(site_id, year))
 
-#total count of urchins spawned for each ecosystem state
-total_counts<- spawn_working%>%
-  data.frame()%>%
-  filter(!is.na(State),!is.na(Sex))%>%
-  filter(Spawn_Mass_false != 0)%>%
-  filter(!(Sex == "NA"))%>%
-  mutate(State=factor(State),
-         Sex=factor(Sex))%>%
-  group_by(State)%>%
-  summarize(total_n=n())
+quad_working <- quad_data %>% 
+  mutate(year = year(survey_date)) %>% 
+  mutate(site_id = paste(site, site_type,zone))
 
-#total count of each sex spawned for each ecosystem state
-sex_counts<- spawn_working%>%
-  data.frame()%>%
-  filter(!is.na(State),!is.na(Sex))%>%
-  filter(Spawn_Mass_false != 0)%>%
-  filter(!(Sex == "NA"))%>%
-  mutate(State=factor(State),
-         Sex=factor(Sex))%>%
-  group_by(State,Sex)%>%
-  summarize(sex_counts=n(),.groups="drop")%>%
-  left_join(total_counts,by="State")%>%
-  mutate(prop= (sex_counts/total_n))
-
+quad_joined <- left_join(quad_working, patch_types, by = "site_id") %>% 
+  mutate(site_type.x = if_else(year == 2024, `2024`, `2025`)) %>% 
+  mutate(site_id = paste(site_id, year))
 
 # Figures -----------------------------------------------------------------
 
-#boxplot overall spawn mass per ecosystem state
-ggplot(data = spawn_working, aes(x = State, y = Spawn_Mass_false)) +
-  geom_boxplot() +
-  #facet_wrap(~ State) +
-  labs(
-    x = "Ecosystem State",
-    y = "Mass (g) ",
-    fill = "Sex") +
-  theme_classic()
-
-#boxplot spawn mass per ecosystem state by sex
-ggplot(data = spawn_working %>% 
-         filter(Spawn_Mass_false != 0)
-       , aes(x = State, y = Spawn_Mass_g)) +
-  geom_boxplot() +
-  facet_wrap(~ Sex) +
-  labs(
-    x = "Ecosystem State",
-    y = "Mass (g) ",
-    fill = "Sex") +
-  theme_classic()
-
-#boxplot 24hr urchin mass per ecosystem state by sex
-ggplot(data = spawn_working, aes(x = Sex, y = Animal_24hr_Mass_g, fill = Sex)) +
-  geom_boxplot() +
-  facet_wrap(~ State) +
-  labs(
-    x = "Sex",
-    y = "Mass (g) ",
-    fill = "Sex") +
-  theme_classic()
-
-#sex distribution for each state 
-ggplot(data = sex_counts
-       , aes(x = State, y = prop, fill=Sex)) +
-  geom_col() +
-  #facet_wrap(~ Sex) +
-  labs(
-    x = "Ecosystem State",
-    y = "Percent (%) ",
-    fill = "Sex") +
-  theme_classic()
-
-#urchin sizefq for site/type/zone (just first zone in dataset)
-ggplot(urchin_sizefq [1:18,],
-       aes(x=size_cm, y=count))+
-  geom_col(fill="skyblue3")+
-  theme_classic()
-
 #urchin sizefq for site/type/zone 
-ggplot(urchin_sizefq_1,
+ggplot(urchin_sizefq_joined,
        aes(x=size_cm, y=count))+
   facet_wrap(~site_id)+
   geom_col(fill="skyblue3")+
   theme_classic()
 
 #urchin diameter x gonad mass for site/type/zone (y=mx+b)
-ggplot(gonad_working,
-       aes(x=Test_Diameter_mm, y=Gonad_Mass_g))+
+ggplot(gonad_patch_joined,
+       aes(x=test_diameter_mm, y=gonad_mass_g))+
   facet_wrap(~site_id)+
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, color = "aquamarine4")+
@@ -154,10 +110,6 @@ ggplot(sampled_urchins, aes(x = sampled_sizes, fill = site_id)) +
 
 
 # Stats -------------------------------------------------------------------
-
-#y=mx+b for the first set of site/type/zone 
-regression <- lm(Gonad_Mass_g ~ Test_Diameter_mm, data = gonad_working [1:27,])
-summary(regression)
 
 #avg urchin density per site/type/zone 
 avg_urchin_density <- quad_data %>% 
